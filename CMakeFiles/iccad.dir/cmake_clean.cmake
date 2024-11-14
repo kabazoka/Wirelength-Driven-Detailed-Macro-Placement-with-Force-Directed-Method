5ae315968f7b6cf050bf8491f7f9db0b91598eae
kabazoka <73285479+kabazoka@users.noreply.github.com>
2024-11-14 16:23:13 +0800

    Cleaned unnecessary files

---
+++ CMakeFiles/iccad.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iccad.dir/src/main.cpp.o"
+  "CMakeFiles/iccad.dir/src/main.cpp.o.d"
+  "iccad"
+  "iccad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iccad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
